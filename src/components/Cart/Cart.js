6582d9e68e7cec6de68e7a3662fca3585a0d1d41
mohammadriad <riadahmmed289@gmail.com>
2020-09-14 16:36:10 +0600

    edit is done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -7,18 +7,22 @@
      for( let i = 0; i<cart.length; i++){
          const product = cart[i];
          total = total + product.price;
-     }
-     let shipping = 12.99;
-     if (total > 35){
+    }
+    let shipping = 0;
+    if (total > 35){
          shipping = 0;
-     }
-      else if( total > 15 ){
+    }
+    else if( total > 15 ){
         shipping = 4.99;
-      }
-      const tax = (total/10).toFixed(2);
-      const grandTotal = (total + shipping + Number(tax).toFixed(2));
-     const formatNumber = num => {
-         const precision = num.toFixed(2);
+    }
+    else if (total > 0){
+        shipping = 12.99
+    }
+    const tax = (total/10).toFixed(2);
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
+    
+    const formatNumber = num => {
+    const precision = num.toFixed(2);
          return Number(precision);
      }
     return (
